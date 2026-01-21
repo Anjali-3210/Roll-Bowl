4e4c37e3253ccd7085f3544c40e81f7438940140
Anjali-3210 <anjalip3210@gmail.com>
2026-01-21 10:18:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -308,7 +308,6 @@ app.get("/admin/tomorrow-users", async (req, res) => {
 });
 
 
-
 app.get("/admin", async (req, res) => {
   const adminKey = req.query.key;
 
@@ -458,6 +457,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
